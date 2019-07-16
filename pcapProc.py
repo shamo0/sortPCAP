25c845a6776c12f06f857ad3236544c8f995b76a
Geno Shamugia <48299520+shamo0@users.noreply.github.com>
2019-07-16 16:21:05 -0400

    Update pcapProc.py

--- pcapProc.py
+++ pcapProc.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-class CAPTURE:
+class PACKET:
     def __init__(self,order,time,source,destination,protocol,length):
         self.order = order
         self.time = time
@@ -9,46 +9,66 @@ class CAPTURE:
         self.protocol = protocol
         self.length = length
 
+# class Process:
+#     def __init__(self,key):
+#         self.key = key
+#
+#     def __lt__(self, other) :
+#         if self.key < other.key:
+#             return True
+#         return False
+
 class PCAP:
     def __init__(self):
-        # self.order = []
-        # self.time = []
-        # self.source = []
-        # self.destination = []
-        # self.protocol = []
-        # self.length = []
         self.container = []
+        self.dict = {}
 
-    def mergeSort(self,list):
-        if len(list) <= 1 :
-            return list
+    def mergeSort(self,listFull):
+        if (len(listFull) <= 1) :
+            return listFull
         else:
-            listFirst = list[:len(list)/2]
-            listSecond = list[len(list)/2:]
-            listFirst = mergeSort(listFirst)
-            listSecond = mergeSort(listSecond)
-            list = merge(listFirst,listSecond)
+            mid =len(listFull)//2
+            LeftList = listFull[:mid]
+            RightList = listFull[mid:]
+            RightList = self.mergeSort(RightList)
+            LeftList = self.mergeSort(LeftList)
+            listFull = self.merge(listFull,LeftList,RightList)
+
+    def merge(self,listFull,LeftList,RightList):
+        m=n=k=0
+        while (m < len(LeftList)) and (n<len(RightList)):
+            if LeftList[m]<RightList[n]:
+                listFull[k]=LeftList[m]
+                m+=1
+            else:
+                listFull[k]=RightList[n]
+                n+=1
+            k+=1
+        while (m < len(LeftList)):
+            listFull[k] = LeftList[m]
+            m+=1
+            k+=1
+        while (n < len(RightList)):
+            listFull[k] = RightList[n]
+            n+=1
+            k+=1
+        return listFull
+
 
     def appendCSV(self,filename):
         fd = open(filename,'r')
         file_read = fd.readline()
         while file_read!="":
             lineSplit = file_read.split(",")
-            self.container.append(CAPTURE(lineSplit[0].strip("\""),lineSplit[1].strip("\""),lineSplit[2].strip("\""),lineSplit[3].strip("\""),lineSplit[4].strip("\""),lineSplit[5].strip("\"\n")))
-            # self.order.append(lineSplit[0].strip('"'))
-            # self.time.append(lineSplit[1].strip('"'))
-            # self.source.append(lineSplit[2].strip('"'))
-            # self.destination.append(lineSplit[3].strip('"'))
-            # self.protocol.append(lineSplit[4].strip('"'))
-            # self.length.append(lineSplit[5].strip('\"\n'))
+            self.container.append(PACKET(lineSplit[0].strip("\""),lineSplit[1].strip("\""),lineSplit[2].strip("\""),lineSplit[3].strip("\""),lineSplit[4].strip("\""),lineSplit[5].strip("\"\n")))
             file_read = fd.readline()
-        print(self.container)
-    def sourceByPackets(self):
-        ...
-        # List = []
-        # for item in self.source:
-        #     List.append((item,self.length[self.source.index(item)]))
 
+    def sourceByPackets(self):
+        newList = []
+        for i in range(len(self.container)-1):
+            # newList.append((self.container[i].source,self.container[i].length))
+            newList.append((self.container[i].length))
+        someList = self.mergeSort(newList)
 
     def sourceByBytes(self):
         ...
@@ -65,7 +85,7 @@ class PCAP:
 def main():
     PCtest = PCAP()
     PCtest.appendCSV('test.csv')
-    # PCtest.sourceByPackets()
+    PCtest.sourceByPackets()
 
 
 
